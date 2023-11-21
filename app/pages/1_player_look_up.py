1ccb05ea5b621786fe4b26f9be77427d7a2429ba
DinoLala <susan2004.nguyen@gmail.com>
2023-11-21 14:57:03 -0500

    test module

--- app/pages/1_player_look_up.py
+++ app/pages/1_player_look_up.py
@@ -3,13 +3,11 @@ from importlib import import_module
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-# from app.common.search import process_html, get_player,get_tournaments,get_norm_summary,get_norm
+# from app.common.search import process_html, get_player,get_tournaments,get_norm_summary
 import streamlit as st
 
-
 import requests
-from bs4 import BeautifulSoup
-import pandas as pd
+
 
 class process_html(object):
     def read_text(self, html_file):
@@ -188,12 +186,11 @@ def get_norm(opponent_list,n_win):
             norm_dict[c]='no'
     return norm_dict
 
-
-st.set_page_config(layout="centered")
+st.set_page_config(layout="wide")
 
 st.sidebar.header(':orange[USCF Norms sytem:]' )
-st.sidebar.write('* Rating 2400: Life Senior Master' )
-st.sidebar.write('* Rating 2200: Life Master ' )
+st.sidebar.write('* Rating 2400: Life Senior Master (S)' )
+st.sidebar.write('* Rating 2200: Life Master (M) ' )
 st.sidebar.write('* Rating 2000 Candidate Master (C) ' )
 st.sidebar.write('* Rating 1800: 1st Category (1)' )
 st.sidebar.write('* Rating 1600: 2nd Category (2)' )
@@ -201,114 +198,108 @@ st.sidebar.write('* Rating 1400: 3rd Category (3)' )
 st.sidebar.write('* Rating 1200: 4th Category (4)' )
 
 
+col1, col2 = st.columns(2)
+with col1:
 
-st.subheader(':orange[Estimating your post tournament rating:]' )
+    st.image("app/data/chess.png")
+    # st.image("app/data/chess2.png")
+    uscf_id=st.text_input('USCF_ID' ,value='')
+    
+with col2:
 
-url = "http://www.glicko.net/ratings/approx.pdf"
+    # st.header(":orange[PLAYER LOOK UP]")
+    # st.title(":orange[!!!]")
+    # st.title(":orange[Never gonna give you up, Never gonna let you down!!!]")
+    st.title(":orange[Happy player!!!]")
+
+    # st.title(":orange[This is for my beloved son. Have Funs !!!]")
+    col11, col21,col23 = st.columns(3)
+    # with col21 :
+    #     st.image("app/data/happy-dance-excited.gif")
+    
+
+h=process_html()
+# try:
+#     uscf_id=str(int(uscf_id))
+# except: 
+#     uscf_id='12743305'
+
+submited=st.button('Find player')
+url = "https://new.uschess.org/players/search"
 # st.write("check out this [link](%s)" % url)
-st.write(":orange[Visit [website ](%s) for rating estimation formula]" % url)
+st.write(":orange[Visit [website ](%s) for official player rating look up]" % url)
 
+if submited and uscf_id !="":
 
-# n_win=st.number_input('your pointstest',min_value=0, max_value=3,step=.5)
-with st.container():
-    # st.write("This is inside the container")
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        current_rating=st.number_input('Your current rating',min_value=.00, max_value=3000.01, )
-    with col2:
-        n_prior=st.number_input(' #Prior games',min_value=.00, max_value=3000.01,value=50.0,step=1.0 )   
-    
     st.divider() 
-    st.write(':orange[Tournament result:]' )
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        # current_rating=st.number_input('Your current rating',min_value=.00, max_value=3000.01, )
-        tour_rounds=st.number_input('#Tournament games',min_value=.00, max_value=20.01, step=1.0 )
-        # tour_rounds=int(tour_rounds)
-    with col2:   
-        n_win=st.number_input('Your points',min_value=0.0, max_value=tour_rounds,step=.5)
-   
+    st.header(":orange[Player Summary !]")
+    # st.markdown(
+    #     ":orange[Player Summary !]"
+    # )
 
-    st.write(':orange[Your opponents rating:]' )
-    col1, col2, col3 = st.columns(3)
-    opponent_list=[]
-    with col1:
-        #    st.header("A cat")
-        
-        for r in range(int(tour_rounds/3)+1):
-            if r*3+1 <=tour_rounds:
-                intput_rating1=st.number_input('rate'+str(r*3+1),min_value=0, max_value=3000, step=1 )
-                opponent_list.append(intput_rating1)
-
-
-    with col2:
-        for r in range(int(tour_rounds/3)+1):
-            if r*3+2 <=tour_rounds:
-                intput_rating1=st.number_input('rate'+str(r*3+2),min_value=0, max_value=3000, step=1 )
-                opponent_list.append(intput_rating1)
-        
-
-    with col3:
-        for r in range(int(tour_rounds/3)):
-            if r*3+3 <=tour_rounds:
-                intput_rating1=st.number_input('rate'+str(r*3+3),min_value=0, max_value=3000, step=1 )
-                opponent_list.append(intput_rating1)
-    
-    est_submit=st.button('Est Post Rating')
-    st.divider() 
-    if est_submit:
-        
-        if current_rating >=2355:
-            N_e=n_prior
-        elif current_rating >0 and current_rating <2355:
-            N_e=min(50/pow(.662+ 0.00000739*pow(2569 - current_rating,2),.5),n_prior)
-        else:
-            current_rating =0
-            N_e=0
-        m=tour_rounds
-        K=800/(N_e+m)
-        S=n_win
-        E=0
-        for p in opponent_list:
-            e_p=1/(1+pow(10,-(current_rating-p)/400))
-            E=E+e_p
-        # get Bonus B
-        if tour_rounds <3:
-            B=0
-        else:
-            temp=K*(S-E)
-            temp2=14*pow(max(m,4),.5)
-            B= max(0, temp-temp2)
-
-        final_est_post=int(current_rating+K*(S-E)+B)
-        # avg_opp=sum(opponent_list)/len(opponent_list)
-        # post_est=(N*int(current_rating)+sum(opponent_list)+(2*n_win-tour_rounds)*400)/(N+tour_rounds)
-
-        # st.write(avg_opp,post_est,tour_rounds)
-
+    with st.container():
+        # st.write("This is inside the container")
         col1, col2, col3 = st.columns(3)
+        
+
+        dict_out=get_player(h,uscf_id)
+        # dict_out['Name']=Name
+        # dict_out['State']=State
+        # dict_out['Gender']=Gender
+        # dict_out['Junior_Ranking']=Junior_Ranking
+            # dict_out['Junior_Ranking']=Junior_Ranking
+        # dict_out['Over_Ranking']=Over_Ranking
+        # dict_out['State_Ranking']=State_Ranking
+        # dict_out['title_name']=title_name
+        # dict_out['current_rating']=current_rating
+
         with col1:
-            st.write(f'Winning expectation: :orange[{round(E,2)}    ] ')
-            if B>0:
-                st.write(f'Expected bonus points: :orange[{int(B)}    ] ')
-        with col2:    
-            norm_dict=get_norm(opponent_list,n_win)
-            max_norm=0
-            for k, t in norm_dict.items():
-                
-                if t=='yes' and max_norm <k:
-                    max_norm=k
-                
-            if max_norm>0:
-                st.write(f'Expected norm {max_norm}: :orange[{norm_dict[max_norm]}]')
-
-        
-        st.header(f'Your estimated post tournament rating is :orange[{final_est_post}    ] ')
-
-        
-
-
-        st.divider() 
-
+            #    st.header("A cat")
+            st.write(dict_out['Name'])
+            # st.write('Gender:',dict_out['Gender'])
+            st.write('State:',dict_out['State'])
+            st.write('Current Title:',dict_out['title_name'])
 
             
+            
+            
+            #    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+        with col2:
+            #    st.header("A dog")
+            # st.write('State:',dict_out['State'])
+            
+            st.write('Current USCF Rating:', dict_out['current_rating'])
+            st.write('Next month USCF Rating:', dict_out['nextmonth_rate'])
+            
+            #    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+        with col3:
+            #    st.header("An owl")
+            st.write('Overall Ranking:', dict_out['Over_Ranking'])
+            st.write('State Ranking:', dict_out['State_Ranking'])
+            st.write('Junior Ranking:', dict_out['Junior_Ranking'])
+
+
+    norm_df=get_norm_summary(h,uscf_id)
+    st.write(':orange[Lastest Norm:]')
+    if len(norm_df)==0:
+        st.write('This player has no norm yet!')
+    else:
+        norm_df=norm_df.sort_values(by=['level'])
+        st.dataframe(norm_df.tail(1))
+    # st.markdown("""<hr style="height:10px;border:none;color:#333;background-color:#333;" /> """, unsafe_allow_html=True)
+    st.divider() 
+
+    # st.write('Lastest Tournaments:':orange[orange])
+
+    st.header(":orange[Lastest Tournaments!]")
+    # st.markdown(
+    #     ":orange[Lastest Tournaments !]"
+    # )
+
+
+
+    html_tables=get_tournaments(h,uscf_id)
+    st.dataframe(html_tables, width=1600, height=300)
+
