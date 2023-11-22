4fbdfda340905a04eb266b74dcce5f6546a27521
DinoLala <susan2004.nguyen@gmail.com>
2023-11-22 16:00:44 -0500

    next button puzzle

--- app/pages/3_Fun_puzzles.py
+++ app/pages/3_Fun_puzzles.py
@@ -13,38 +13,40 @@ st.set_page_config(layout="wide")
 
 st.header(':orange[Have fun solving puzzles with us !]')
 
-col1, col2 = st.columns(2,gap="medium")
-with col1:
-    # st.image("app/data/chess2.png")
-    st.write(':blue[White to move and draw]')
-    st.image("app/data/puzzles/1.png")
-
-    
-with col2:
-
-    # st.header(":orange[PLAYER LOOK UP]")
-    # st.title(":orange[!!!]")
-    # st.title(":orange[Never gonna give you up, Never gonna let you down!!!]")
-    # st.title(":orange[Happy player!!!]")
-    st.write(':blue[White to move and win]')
-    st.image("app/data/puzzles/2.png")
-   
-col1, col2 = st.columns(2,gap="medium") 
-with col1:
-    # st.image("app/data/chess2.png")
-
-    st.write(':blue[White to move and win]')
-    st.image("app/data/puzzles/3.png")
-
-    
-with col2:
-
-    # st.header(":orange[PLAYER LOOK UP]")
-    # st.title(":orange[!!!]")
-    # st.title(":orange[Never gonna give you up, Never gonna let you down!!!]")
-    # st.title(":orange[Happy player!!!]")
-   
-    st.write(':blue[White to move and win]')
-    st.image("app/data/puzzles/4.png")
-
+st.header('')
             
+import os
+
+col1,col2 = st.columns(2)
+
+if 'counter' not in st.session_state: 
+    st.session_state.counter = 0
+
+def showPhoto(photo):
+    # col2.image(photo,caption=photo)
+    col2.image(photo)
+    # col1.write(f"Index as a session_state attribute: {st.session_state.counter}")
+    if 'ww' in photo:
+        col1.subheader(f"White to move and win")
+    elif "wd" in photo:
+        col1.subheader(f"White to move and draw")
+
+    
+    ## Increments the counter to get next photo
+    st.session_state.counter += 1
+    if st.session_state.counter >= len(pathsImages):
+        st.session_state.counter = 0
+
+# Get list of images in folder
+# folderWithImages = r"images"
+folderWithImages = r"app/data/puzzles"
+
+pathsImages = [os.path.join(folderWithImages,f) for f in os.listdir(folderWithImages)]
+
+# col1.subheader("List of images in folder")
+
+# col1.write(pathsImages)
+
+# Select photo a send it to button
+photo = pathsImages[st.session_state.counter]
+show_btn = col1.button("Show next puzzle ⏭️",on_click=showPhoto,args=([photo]))
